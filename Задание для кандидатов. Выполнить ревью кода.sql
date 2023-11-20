d4a8f245a7df4a19fe4d3c15f158e86f6e3e82b0
Lyaysan88 <112409675+Lyaysan88@users.noreply.github.com>
2023-11-20 20:46:19 +0500

    3_Комментарий
    На одном уровне с `if` и `begin/end`

--- Задание для кандидатов. Выполнить ревью кода.sql
+++ Задание для кандидатов. Выполнить ревью кода.sql
@@ -12,7 +12,7 @@ begin
 	where f.ID = @ID_Record
 		and f.FlagLoaded = cast(1 as bit)
 	)
-		begin
+        begin
 			set @ErrorMessage = 'Ошибка при загрузке файла, проверьте корректность данных'
 
 			raiserror(@ErrorMessage, 3, 1)
